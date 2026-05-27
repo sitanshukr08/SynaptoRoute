ffb8ec22830d51daba02b3bfb658f3080b702917
Sitanshu <sitanshukr08@users.noreply.github.com>
2026-05-27 19:15:02 +0530

    fix: expose core components in package __init__ to fix API imports

--- src/synaptoroute/__init__.py
+++ src/synaptoroute/__init__.py
@@ -1,1 +1,6 @@
-# Init file
+from synaptoroute.router import AdaptiveRouter
+from synaptoroute.encoder import Encoder
+from synaptoroute.storage import BaseStorage, SQLiteStorage
+from synaptoroute.models import Route
+
+__all__ = ["AdaptiveRouter", "Encoder", "BaseStorage", "SQLiteStorage", "Route"]
